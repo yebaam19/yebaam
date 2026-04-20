2396b9cc5bd393f5a44c7780878d4a2b1390b4c4
ellis velandia <eyis619@gmail.com>
2026-04-20 15:20:27 -0500

    layout ask me

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 import Header from '@/components/Header/Header'
 import { ApplicationLayout } from './application-layout'
 
-export default function Layout({ children, params }: { children: React.ReactNode; params: any }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return <ApplicationLayout header={<Header hasBorderBottom={false} />}>{children}</ApplicationLayout>
 }
